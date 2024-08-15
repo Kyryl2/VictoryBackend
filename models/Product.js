4a795017c5c8ce89b35256bf354c450c1f121f7e
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 01:36:20 +0200

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,23 @@
+// const mongoose = require('mongoose');
+
+// const ProductSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   category: { type: String, required: true }, // 'pizza', 'sushi', etc.
+//   price: { type: Number, required: true },
+//   description: { type: String },
+//   image: { type: String }
+// });
+
+// const Product = mongoose.model('Product', ProductSchema);
+// module.exports = Product;
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true }, // 'pizza', 'sushi', etc.
   price: { type: Number, required: true },
-  description: { type: String },
-  image: { type: String }
+  // Інші поля
 });
 
-const Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model('Product', productSchema);
+
 module.exports = Product;
