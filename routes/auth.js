53251b6be5ee815905654ef15eacf65df9c04f05
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 16:57:33 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    let user = await User.findOne({ email });
+    let user = await User.findUser({ email });
     if (user) return res.status(400).json({ msg: 'User already exists' });
 
     user = new User({ name, email, password });
