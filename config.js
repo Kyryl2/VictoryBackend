38ece12cd830aec9d2b60ba6f5ef2d117bccf806
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 23:30:12 +0200

    ff

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    port: process.env.PORT || 5000
-  };
-  
--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import Order from '../models/Order.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
-
+ import authMiddleware from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // Додати товар до кошика
