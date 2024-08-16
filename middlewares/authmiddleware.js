d14ba2f47b12bf7c43054f7ec77d27931c69b14e
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 16:51:04 +0200

    Update authmiddleware.js

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 // const { jwtSecret }= require('../config') ;
 
-module.exports = function (req, res, next) {
+module.exports = function (req, res, next) {  
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
@@ -13,3 +13,4 @@ module.exports = function (req, res, next) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
+    
