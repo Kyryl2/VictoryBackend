fc1144d847a255d41705b42ad1ef3f021190865b
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 17:40:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const app = express();
 
 
 // Підключення до MongoDB
-mongoose.connect(process.env.MONGO_URI)
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('MongoDB Connection Error:', err));
 
 // Middleware
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
+
 app.use(express.json());
 
 // Роутинг
