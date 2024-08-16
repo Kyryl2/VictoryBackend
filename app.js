90ef31a7ed1c816f6e949efc55d1326a9a0d2218
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 22:25:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { mongoURI } = require('./config');
 require('dotenv').config();
 const cors = require('cors');
+const User = require('./models/User');
 
 
 const app = express();
@@ -23,28 +24,43 @@ app.use(express.json());
 // Роутинг
 app.post('/register', async (req, res, next) => {
 
-    const { email, password, name } = req.body;
-    const existingUser = await User.findOne({ email });
+  const { name, email, password } = req.body;
 
-    if (existingUser) {
-      return res.status(409).json({ message: "Email in use" });
-    }
+  const user = await User.findOne({ email });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashedPassword, name });
+  if (user) {
+    throw new HttpError(409, 'Email already in use');
+  }
 
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,  // Залиште або видаліть це, залежно від схеми
-      },
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const verificationToken = nanoid(12);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashPassword,
+    verificationToken,
+  });
+
+
+
+  res.json({
+    status: 201,
+    message: 'User successfully registered',
+    data: {
+      username: newUser.username,
+      email: newUser.email,
+    },
+  });
+
     });
   // } catch (error) {
   //   console.error('Error during registration:', error.message);  // Логування помилки
   //   res.status(500).send('Server error');
   // }
-});
-app.use('/api/auth', require('./routes/auth.js'));
+
+// app.use('/api/auth', require('./routes/auth.js'));
 app.use('/api/products', require('./routes/products.js'));
 app.use('/api/orders', require('./routes/orders.js'));
 // app.post('/register', (req, res) => {
