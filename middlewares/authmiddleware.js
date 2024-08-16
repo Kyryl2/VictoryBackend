566ab681d3a9ca7b0139c5559e2b1da69d823e79
Kyryl <krasiuk.4884@gmail.com>
2024-08-16 23:23:46 +0200

    Update authmiddleware.js

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../middlewares/authMiddleware.js';
+import User from '../models/User.js';
 
 const authMiddleware = async (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
